a9d9046453da471be2c4ddf7de14b112e965e7e1
Hugo Gonzalez Besada <hugogbmahu@gmail.com>
2025-05-07 13:39:28 +0200

    feat(RetailPrice): introducir el metodo contains y cambiar chese por cheese :=)

--- src/main/java/edu/estatuas/RetailPrice.java
+++ src/main/java/edu/estatuas/RetailPrice.java
@@ -16,7 +16,7 @@ public class RetailPrice {
         prices.put("large", 0.50);
         prices.put("sauce", 0.50);
         prices.put("medium", 0.25);
-        prices.put("chese", 0.25);
+        prices.put("cheese", 0.25);
     }
 
     public static void display() {
@@ -27,6 +27,15 @@ public class RetailPrice {
 
     public static void display(Product product){
         System.out.println(product);
+    }
+
+    public static double contains(String extra) {
+        double price = 0;
+        for (Map.Entry<String, Double> entry : prices.entrySet()) {
+            if (entry.getKey().equals(extra)) {
+                price = entry.getValue();
+            }
+        } return price;
     }
 
 
