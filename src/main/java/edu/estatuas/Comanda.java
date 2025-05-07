61706b810f7d219a465d60be173f3a8ea592eda5
Hugo Gonzalez Besada <hugogbmahu@gmail.com>
2025-05-07 13:57:29 +0200

    feat(Comanda): añadir un nuevo metodo addItem

--- src/main/java/edu/estatuas/Comanda.java
+++ src/main/java/edu/estatuas/Comanda.java
@@ -6,4 +6,5 @@ public interface Comanda{
     void addItem(String name ,double price);
     void display();
     List<Item> getItems();
+    void addItem(String item1, double price, String item2);
 }
